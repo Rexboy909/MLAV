0de5ac3765435f31d368867357ada87f01c31fd0
Rex <ulissespineda84@gmail.com>
2025-12-10 19:01:12 -0700

    change ui a smidge

--- src/ui.rs
+++ src/ui.rs
@@ -1,20 +1,17 @@
 use macroquad::prelude::*;
 
-struct Colors {
-    bg_color: Color,
-    element_color: Color,
-}
-/*let ui_colors = Colors {
-    bg_color: BLACK,
-    element_color: GRAY,
-};
-*/
+//--colors--//
+
+const BG_COLOR: Color = Color::new(53.0/255.0, 16.0/255.0, 108.0/255.0, 1.0);
+const SIDEBAR_BG_COLOR: Color = Color::new(100.0/255.0, 100.0/255.0, 100.0/255.0, 1.0);
+
+//--main ui drawing function--//
 pub fn draw_main_ui() {
 
-    clear_background(BLACK);
+    clear_background(BG_COLOR);
     
     // --sidebar--//
     // for some reason, there is already a 30px margin?, so add another 30.0
-    draw_rectangle(30.0, 30.0, 120.0, screen_height() - 60.0, LIGHTGRAY); 
+    draw_rectangle(30.0, 30.0, 120.0, screen_height() - 60.0, SIDEBAR_BG_COLOR); 
     //draw_rectangle_lines(x, y, w, h, thickness, color); // doubled up for rectangle border
 }
