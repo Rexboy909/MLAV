cb6eb6947147c6051598379a3da81fdb9d1abee9
Rex <ulissespineda84@gmail.com>
2026-02-23 11:19:49 -0700

    Added a logo. not implemente y

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 mod ui;
 use macroquad::prelude::*;
 
-#[macroquad::main("MLAV")]
+#[macroquad::main(window_conf)]
 async fn main() {
-    clear_background(BLACK);
     loop {
         ui::draw_main_ui();
-
         next_frame().await;
+    }
+}
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "MLAV".to_owned(),
+        window_width: 1280,
+        window_height: 720,
+        platform: miniquad::conf::Platform {
+            swap_interval: Some(0),
+            ..Default::default()
+        },
+        ..Default::default()
     }
 }
